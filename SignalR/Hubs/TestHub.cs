fe1bc74a5b5716dae044bdab1fd7337dc633ad05
teymur75 <teymurvr@code.edu.az>
2024-02-14 16:46:41 +0400

    trigger functions on client

--- SignalR/Hubs/TestHub.cs
+++ SignalR/Hubs/TestHub.cs
@@ -4,9 +4,25 @@ namespace SignalR.Hubs
 {
     public class TestHub:Hub
     {
+        public static List<string> clients=new List<string>();
         public async Task SendMessageAsync(string message)
         {
             await Clients.All.SendAsync("ReceivedMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            //loglama ucun en elverisli variyantdir
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userLeft", Context.ConnectionId);
         }
     }
 }
